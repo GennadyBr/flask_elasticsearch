003bf1fe73d4ad46f200fa2ec1635b54fa620459
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-08 19:51:43 +0300

    База уже содержит {doc_count} документов

--- loader.py
+++ loader.py
@@ -1,8 +1,10 @@
+import logging
+
 from elasticsearch import Elasticsearch
 import csv
 import os
 from dotenv import load_dotenv
-from settings import logger
+from settings import logger, index_name
 
 load_dotenv()
 
@@ -16,17 +18,24 @@ url = f"http://{os.getenv('ES_HOST')}:9200"
 es = Elasticsearch(hosts=[url])
 logger.info(f'Connecting to Elasticsearch cluster `{es.info().body["cluster_name"]}`')
 
-with open('Car_details_v3.csv', 'r') as f:
-    reader = csv.reader(f)
-    flog(next(reader))
 
-    for i, line in enumerate(reader):
-        document = {
-            "name": line[0],
-            "engine": line[9],
-            "year": line[1],
-            "price": line[2]
-        }
-        flog(document)
-        res = es.index(index="cars", document=document)
-        flog(res)
+doc_count = es.count(index=index_name)['count']
+if not doc_count:
+    with open('Car_details_v3.csv', 'r') as f:
+        reader = csv.reader(f)
+        flog(next(reader))
+
+        for i, line in enumerate(reader):
+            document = {
+                "name": line[0],
+                "engine": line[9],
+                "year": line[1],
+                "price": line[2]
+            }
+            flog(document)
+            res = es.index(index=index_name, document=document)
+            flog(res)
+    doc_count = es.count(index=index_name)['count']
+    logger.info(f'В базу добавлено {doc_count} документов')
+else:
+    logger.info(f'База уже содержит {doc_count} документов')
--- settings.py
+++ settings.py
@@ -23,3 +23,5 @@ logging_conf = {
 
 logging.config.dictConfig(logging_conf)
 logger = logging.getLogger("app_logger")
+
+index_name = "cars"

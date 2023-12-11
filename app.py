3a0a030edfa2003d915735d0df2345a1aeed407d
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-11 15:06:49 +0300

    return "HIHIHIHIHIHIHIHIHI"

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # @app.route('/')
 @app.route('/search')
 def search_autocomplete():
-    return ""
+    return "HIHIHIHIHIHIHIHIHI"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('FLASK_PORT'), debug=True)

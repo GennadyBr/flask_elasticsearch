03c5e7749bba7ced0833b7638c15071e423b4761
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-11 20:11:07 +0300

    except BadRequestKeyError

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, request
 from dotenv import load_dotenv
+from werkzeug.exceptions import BadRequestKeyError
 
 from db.es import es_conn, get_payload
 from logger.settings import logger
@@ -14,13 +15,11 @@ max_size = 100
 
 @app.route('/search')
 def search_autocomplete():
-    query = request.args["q"].lower()
-    logger.info(f'{query=}')
-    tokens = query.split(" ")
-    logger.info(f'{tokens=}')
-    if len(tokens) < 1:
-        return "Please provide an autocomplete query"
-    else:
+    try:
+        query = request.args["q"].lower()
+        logger.info(f'{query=}')
+        tokens = query.split(" ")
+        logger.info(f'{tokens=}')
         payload = get_payload(tokens)
         logger.info(f'{payload=}')
         es = es_conn()
@@ -28,7 +27,8 @@ def search_autocomplete():
         response = es.search(index="cars", query=payload, size=max_size)
         logger.info(f"{response['hits']['hits']=}")
         return [result['_source']['name'] for result in response['hits']['hits']]
-
+    except BadRequestKeyError:
+        return "Please provide an autocomplete query"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('FLASK_PORT'), debug=True)

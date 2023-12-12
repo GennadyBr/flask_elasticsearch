b6b6338d88af70cbe92e77a5e961fe9f9fac6846
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-12 13:27:44 +0300

    elasticsearch==8.11.1

--- db/es.py
+++ db/es.py
@@ -5,15 +5,17 @@ from elasticsearch import Elasticsearch
 from elastic_transport import ConnectionError
 from config.settings import logger, search_field
 
-def es_conn():
+
+def es_conn() -> Elasticsearch:
     url = f"http://{os.getenv('ES_HOST')}:9200"
     try:
-        es = Elasticsearch(hosts=[url])
+        es = Elasticsearch(url)
+        # es = Elasticsearch(hosts=[url])
         logger.info(f'Connecting to Elasticsearch cluster `{es.info().body["cluster_name"]}`')
         return es
     except ConnectionError as err:
         logger.error(f'ConnectionError {err}')
-        raise ConnectionError(f'ConnectionError {err}')
+        # raise ConnectionError(f'ConnectionError {err}')
 
 
 def get_payload(tokens: List[str]) -> dict:

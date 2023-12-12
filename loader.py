c109fbf161a0bc2a2e59abb56977a0c3962b2c0d
GennadyBr <gennady.budazhapov@gmail.com>
2023-12-12 13:37:38 +0300

    _load_data()

--- loader.py
+++ loader.py
@@ -7,31 +7,37 @@ from db.es import es_conn
 
 load_dotenv()
 es = es_conn()
+
+def _load_data():
+    with open('faker_employee_salary.csv', 'r') as f:
+        reader = csv.reader(f)
+        logger.info(f'{next(reader)=}')
+
+        for i, line in enumerate(reader):
+            document = {
+                "Employee ID": line[0],
+                "First Name": line[1],
+                "Last Name": line[2],
+                "Department": line[3],
+                "Salary": line[4],
+                "Joining Date": line[5],
+                "Email ID": line[6],
+                "Address": line[7]
+            }
+            logger.info(f'{document=}')
+            res = es.index(index=index_name, document=document)
+            logger.info(f'{res=}')
+    _doc_count = es.count(index=index_name)['count']
+    logger.info(f'В базу добавлено {_doc_count} документов')
+
+
 try:
     doc_count = es.count(index=index_name)['count']
 except NotFoundError as err:
     logger.error(err)
+    _load_data()
 else:
     if not doc_count:
-        with open('faker_employee_salary.csv', 'r') as f:
-            reader = csv.reader(f)
-            logger.info(f'{next(reader)=}')
-
-            for i, line in enumerate(reader):
-                document = {
-                    "Employee ID": line[0],
-                    "First Name": line[1],
-                    "Last Name": line[2],
-                    "Department": line[3],
-                    "Salary": line[4],
-                    "Joining Date": line[5],
-                    "Email ID": line[6],
-                    "Address": line[7]
-                }
-                logger.info(f'{document=}')
-                res = es.index(index=index_name, document=document)
-                logger.info(f'{res=}')
-        doc_count = es.count(index=index_name)['count']
-        logger.info(f'В базу добавлено {doc_count} документов')
+        _load_data()
     else:
         logger.info(f'База уже содержит {doc_count} документов')
